6d23f3b99f3fd53a5de83daf0503908ff3143d29
Mirko Moschella <shary.mirko@gmail.com>
2022-05-02 17:17:53 +0200

    setto la categoria principale

--- products.php
+++ products.php
@@ -1,9 +1,36 @@
 <?php
 
 class Product {
-    private boolean $available;
+
+    private $available;
     private $price;
     private $stock;
     private $brand;
-    private $
+
+    public function __construct($avail, $price, $stock, $brand){
+        $this->available = $avail;
+        $this->price = $price;
+        $this->stock = $stock;
+        $this->brand = $brand;
+        return $this;
+    }
+};
+
+class CatFood extends Product{
+    private $name;
+    private array $ingrediants;
+
+    public function setName($name) {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function setIngrediants($ing) {
+        $this->ingrediants = $ing;
+        return $this;
+    }
 }
+$lol = new CatFood('true', '12€', 245, 'Purina');
+$lol->setName('TopoVivo');
+$lol->setIngrediants(['Topo', 'Carote', 'Umani']);
+var_dump($lol);
