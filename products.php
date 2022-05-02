90656ecb60ba8c522776b71d5615a02fedc7ae4a
Mirko Moschella <shary.mirko@gmail.com>
2022-05-02 17:23:55 +0200

    aggiungi cibi e giochi

--- products.php
+++ products.php
@@ -2,12 +2,14 @@
 
 class Product {
 
+    private $name;
     private $available;
     private $price;
     private $stock;
     private $brand;
 
-    public function __construct($avail, $price, $stock, $brand){
+    public function __construct($name, $avail, $price, $stock, $brand){
+        $this->name = $name;
         $this->available = $avail;
         $this->price = $price;
         $this->stock = $stock;
@@ -17,20 +19,24 @@ class Product {
 };
 
 class CatFood extends Product{
-    private $name;
     private array $ingrediants;
-
-    public function setName($name) {
-        $this->name = $name;
-        return $this;
-    }
-
     public function setIngrediants($ing) {
         $this->ingrediants = $ing;
         return $this;
     }
 }
-$lol = new CatFood('true', '12€', 245, 'Purina');
-$lol->setName('TopoVivo');
+
+$lol = new CatFood('TopoVivo', 'true', '12€', 245, 'Purina');
 $lol->setIngrediants(['Topo', 'Carote', 'Umani']);
-var_dump($lol);
+
+class Games extends Product{
+    private array $materials;
+    
+    public function setMaterials($materials){
+        $this->materials = $materials;
+    }
+}
+
+$gioco = new Games('Giocattolo', 'true', '32€', 32, 'GiochiRischiosi');
+$gioco->setMaterials(['curaro', 'pelle', 'acidi']);
+var_dump($gioco);
